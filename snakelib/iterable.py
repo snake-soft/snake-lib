391c50c7f8d364be1529e5559484394557760bfa
Snake-Soft <info@snake-soft.com>
2020-07-22 20:38:26 +0200

    iterable fixed

--- snakelib/iterable.py
+++ snakelib/iterable.py
@@ -1,18 +1,11 @@
+from collections import defaultdict
 
-def intersection(*iterables):
+def intersection(iterable1, iterable2):
     """ method to calculate intersection between two lists
     Tested with string, list, set
     If all iterables are of same type (should), it returns items of this type
-    :param iterables: any object that can be iterated
+    :param iterable1: any object that can be iterated
+    :param iterable2: any object that can be iterated
     :returns: set of intersecting elements
     """
-    types = {type(i) for i in iterables}
-    intersected = set.intersection(*[set(iterable) for iterable in iterables])
-    if len(types) is 1:  # all of same type
-        output_type = types.pop()
-        if output_type == str:
-            return ''.join(intersected)
-        elif output_type in (list, set):
-            return output_type(intersected)
-
-    return intersected
+    return {elem for elem in iterable1 if elem in iterable2}
